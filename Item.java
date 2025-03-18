3b5a54d100c29b0e90e26eb006f75c932a9daeba
NBenson4 <nickjsbenson@outlook.com>
2025-03-18 14:06:30 -0400

    Class which implements the 'Item'

---
+++ Item.java
@@ -0,0 +1,14 @@
+public class Item {
+    private String name;
+    private int healingPower;
+
+    public Item(String name, int healingPower) {
+        this.name = name;
+        this.healingPower = healingPower;
+    }
+
+    public String getInfo() {
+        return "Item: " + name +
+                ", Healing Power: " + healingPower;
+    }
+}
